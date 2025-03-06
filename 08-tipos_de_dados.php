c4157aa2972098d72bd2a9a24042016342baf8d2
Guilherme Almeida <guilhermealmeidadaluz5@gmail.com>
2025-03-06 15:40:12 -0400

    Updating

--- 08-tipos_de_dados.php
+++ 08-tipos_de_dados.php
@@ -1,7 +1,66 @@
+Data: 27/02/2025
+
 <?php
+
 /*************** Escalares ******************/
 
-// string
-$nome = "Olá mundo";
 
-var_dump()  // função var_dump mostra informações sobre uma variável
+// string ----------------------------------
+
+$nome = "Olá mundo";    // acento é contabilizado no length da string
+var_dump($nome);  // função var_dump mostra informações sobre uma variável
+echo gettype($nome), "<br>";    // gettype captura o tipo e echo imprime o tipo capturado
+
+if (is_string($nome)):
+    echo "É uma string";
+else:
+    echo "Não é uma string";
+endif;
+echo "<hr>";    // tag para linha
+
+
+// inteiro ----------------------------
+
+$idade = 21;
+var_dump($idade);
+
+if (is_int($idade)):
+    echo "É um inteiro";
+else:
+    echo "Não é um inteiro";
+endif;
+echo "<hr>";
+
+
+// float ----------------------------
+$altura = 1.77;
+var_dump($altura);
+
+if (is_float($altura)):
+    echo "É um float";
+
+else:
+    echo "Não é um float";
+endif;
+echo "<hr>";
+
+
+// boolean ---------------------------
+$admin = false;
+var_dump($admin);
+
+if (is_bool($admin)):
+    echo "É um booleano";
+else:
+    echo "Não é um booleano";
+endif;
+echo "<hr>";
+
+/*************** Compostos ******************/
+
+// array
+
+$carros = ["Hilux", "Montana", "Corolla", 2008, 75100.90, true];
+// $tipos = ("Água", "Óleo");
+var_dump($carros);
+// var_dump($tipos);
